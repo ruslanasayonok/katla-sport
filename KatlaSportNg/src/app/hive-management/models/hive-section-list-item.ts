adc381e2082f8c02a1414f6d2d7950f4e60c2928
Руслан <375293425101@ya.ru>
2018-10-16 14:05:33 +0300

    step_8_finish. Implement HiveServiceSection page.

--- KatlaSportNg/src/app/hive-management/models/hive-section-list-item.ts
+++ KatlaSportNg/src/app/hive-management/models/hive-section-list-item.ts
@@ -1,4 +1,8 @@
 export class HiveSectionListItem {
     constructor(
+        public Id : number,
+        public Name : string,
+        public Code : string,
+        public IsDeleted : boolean
     ) { }
 }
